c6bcb301628dfb64cece3f5c4d59d43a7e507ebf
Rania AbdelMonem Mahmoud <raniimenem@gmail.com>
2022-12-08 15:16:09 +0100

    Delete src directory

--- src/store.ts
+++
@@ -1,9 +0,0 @@
-import { legacy_createStore as createStore} from 'redux';
-import { BookModelUI } from './models/book.model';
-import { bookReducer } from './reducers/book.reducer';
-
-export interface State {
-    bookList: BookModelUI[],
-    searchBooklist: BookModelUI[],
-}
-export const store = createStore(bookReducer);
